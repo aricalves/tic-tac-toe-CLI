8ec22e4a9e294193dfa51ffad94402324b7d5639
Aric Alves <aric.alves@yahoo.com>
2018-01-08 15:29:51 -0800

    Add player, game, and board methods and tests

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
-class Game {
-  
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
 }
 
+class Game {
+  constructor() {
+    this.winnerFound = false;
+    this.winner;
+    this.player1;
+    this.player2;
+  }
+}
+
+Game.prototype.declareWinner = () => this.winner;
+
+Game.prototype.addPlayer = player => {
+  if (this.player1) {
+    this.player2 = new Player(player);
+    return `Welcome ${player}, you are Player 2`;
+  } else {
+    this.player1 = new Player(player);
+    return `Welcome ${player}, you are Player 1`;
+  }
+};
+
+class Board {
+  constructor() {
+    this.board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']];
+  }
+}
+
+Board.prototype.render = function() { return this.board };
+
+// const game = new Game();
+
+const test = new Board()
+console.log(test.render())
 
 exports.Game = Game;
+exports.Board = Board;
+exports.Player = Player;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,32 @@
-const assert = require('chai').assert;
+const { assert, expect } = require('chai');
 const app = require('../app');
 
 describe('Game', () => {
+  const game = new app.Game();
   it('should be an object', () => {
-    const game = new app.Game();
     assert.typeOf(game, 'object');
+  });
+  it('should init without a winner', () => {
+    expect(game.winnerFound).to.eql(false);
+  });
+  it('should not have a winner if the game is still running', () => {
+    expect(game.declareWinner()).to.eql(undefined);
+  });
+});
+
+describe('Player', () => {
+  const player = new app.Player('Aric');
+  it('should be an object', () => {
+    assert.typeOf(player, 'object');
+  });
+  it('should init with a name', () => {
+    expect(player.name).to.eql('Aric');
+  });
+});
+
+describe('Board', () => {
+  const board = new app.Board();
+  it('should be an object', () => {
+    assert.typeOf(board, 'object');
   });
 });
